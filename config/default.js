2d65467ba9ddefab3b14b46b3213256975a21588
Jiaming <Guo>
2018-05-21 21:23:48 +0800

    change "default.js"

---
+++ config/default.js
@@ -0,0 +1,12 @@
+const config = {
+	port: 3000, 
+	database: {
+		DATABASE: 'moments',
+		USERNAME: 'root',
+		PASSWORD: 'mark19980716',
+		PORT: '3002',
+		HOST: 'localhost'
+	}
+}
+
+module.exports = config 
