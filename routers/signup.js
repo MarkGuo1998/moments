c24789bc62bc81270a5549cc9774ae71832a4968
Jiaming <Guo>
2018-05-22 16:54:40 +0800

    change mysql

--- routers/signup.js
+++ routers/signup.js
@@ -3,7 +3,7 @@ const userModel = require('../lib/mysql.js');
 const md5 = require('md5')
 const checkNotLogin = require('../middlewares/check.js').checkNotLogin
 const checkLogin = require('../middlewares/check.js').checkLogin
-const moment = require('moment');
+// const moment = require('moment');
 const fs = require('fs')
 // 注册页面
 router.get('/signup', async(ctx, next) => {
@@ -17,9 +17,10 @@ router.post('/signup', async(ctx, next) => {
 //console.log(ctx.request.body)
 let user = {
     name: ctx.request.body.name,
-    pass: ctx.request.body.password,
+    password: ctx.request.body.password,
     repeatpass: ctx.request.body.repeatpass,
 }
+console.log(user.name);
 await userModel.findDataByName(user.name)
     .then(async (result) => {
         console.log(result)
@@ -35,14 +36,14 @@ await userModel.findDataByName(user.name)
                 data: 1
             };;
 
-        } else if (user.pass !== user.repeatpass || user.pass === '') {
+        } else if (user.password !== user.repeatpass || user.password === '') {
             ctx.body = {
                 data: 2
             };
         } else {
             // ctx.session.user=ctx.request.body.name   
             let getName = Number(Math.random().toString().substr(3)).toString(36) + Date.now()  
-            await userModel.insertData([user.name, md5(user.pass), getName, moment().format('YYYY-MM-DD HH:mm:ss')])
+            await userModel.insertData([user.name, md5(user.password), getName])
                 .then(res=>{
                     console.log('注册成功',res)
                     //注册成功
