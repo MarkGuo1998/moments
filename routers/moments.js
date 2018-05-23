98eeade14a2850324ba6c4f7878110b29b8158b0
Jiaming <Guo>
2018-05-23 17:06:00 +0800

    message

--- routers/moments.js
+++ routers/moments.js
@@ -2,7 +2,6 @@ const router = require('koa-router')();
 const userModel = require('../lib/mysql.js')
 const checkNotLogin = require('../middlewares/check.js').checkNotLogin
 const checkLogin = require('../middlewares/check.js').checkLogin;
-const md = require('markdown-it')();  
 // 重置到文章页
 router.get('/', async(ctx, next) => {
     ctx.redirect('/moments')
@@ -14,42 +13,28 @@ router.get('/moments', async(ctx, next) => {
         name = decodeURIComponent(ctx.request.querystring.split('=')[1]);
     if (ctx.request.querystring) {
         console.log('ctx.request.querystring', name)
-        await userModel.findDataByUser(name)
-            .then(result => {
-                postsLength = result.length
-            })
-        await userModel.findPostByUserPage(name,1)
+        await userModel.findMomentsByName(name)
             .then(result => {
                 res = result
             })
-        await ctx.render('selfPosts', {
+        await ctx.render('selfMoments', {
             session: ctx.session,
-            posts: res,
-            postsPageLength:Math.ceil(postsLength / 10),
+            posts: res
         })
     } else {
-        await userModel.findPostByPage(1)
-            .then(result => {
-                //console.log(result)
-                res = result
-            })
-        await userModel.findAllPost()
+        await userModel.findAllMoments()
             .then(result=>{
-                postsLength = result.length
+                res = result
             })    
-        await ctx.render('posts', {
+        await ctx.render('moments', {
             session: ctx.session,
-            posts: res,
-            postsLength: postsLength,
-            postsPageLength: Math.ceil(postsLength / 10),
-            
+            posts: res
         })
     }
 })
 // 首页分页，每次输出10条
-router.post('/posts/page', async(ctx, next) => {
-    let page = ctx.request.body.page;
-    await userModel.findPostByPage(page)
+router.post('/moments', async(ctx, next) => {
+    await userModel.findAllMoments()
             .then(result=>{
                 //console.log(result)
                 ctx.body = result   
@@ -58,9 +43,9 @@ router.post('/posts/page', async(ctx, next) => {
         })  
 })
 // 个人文章分页，每次输出10条
-router.post('/posts/self/page', async(ctx, next) => {
-    let data = ctx.request.body
-    await userModel.findPostByUserPage(data.name,data.page)
+router.post('/moments/self', async(ctx, next) => {
+    let name = ctx.request.body.name
+    await userModel.findMomentsByName(name)
             .then(result=>{
                 //console.log(result)
                 ctx.body = result   
--- routers/signup.js
+++ routers/signup.js
@@ -15,43 +15,43 @@ router.get('/signup', async(ctx, next) => {
 // post 注册
 router.post('/signup', async(ctx, next) => {
 //console.log(ctx.request.body)
-let user = {
-    name: ctx.request.body.name,
-    password: ctx.request.body.password,
-    repeatpass: ctx.request.body.repeatpass,
-}
-//console.log(user.name);
-await userModel.findUserByName(user.name)
-    .then(async (result) => {
-        //console.log(result)
-        if (result.length) {
-            try {
-                throw Error('用户已经存在')
-            } catch (error) {
-                //处理err
-                console.log(error)
-            }
-            // 用户存在
-            ctx.body = {
-                data: 1
-            };;
-
-        } else if (user.password !== user.repeatpass || user.password === '') {
-            ctx.body = {
-                data: 2
-            };
-        } else {
+    let user = {
+        name: ctx.request.body.name,
+        password: ctx.request.body.password,
+        repeatpass: ctx.request.body.repeatpass,
+    }
+    //console.log(user.name);
+    await userModel.findUserByName(user.name)
+        .then(async (result) => {
+            //console.log(result)
+            if (result.length) {
+                try {
+                    throw Error('用户已经存在')
+                } catch (error) {
+                    //处理err
+                    console.log(error)
+                }
+                // 用户存在
+                ctx.body = {
+                    data: 1
+                };;
+    
+            } else if (user.password !== user.repeatpass || user.password === '') {
+                ctx.body = {
+                    data: 2
+                };
+            } else {
             // ctx.session.user=ctx.request.body.name   
-            let getName = Number(Math.random().toString().substr(3)).toString(36) + Date.now()  
-            await userModel.signUp([user.name, md5(user.password), getName])
-                .then(res=>{
-                    console.log('注册成功',res)
-                    //注册成功
-                    ctx.body = {
-                        data: 3
-                    };
-            })
-        }
-    })
+                let getName = Number(Math.random().toString().substr(3)).toString(36) + Date.now()  
+                await userModel.signUp([user.name, md5(user.password), getName])
+                    .then(res=>{
+                        console.log('注册成功',res)
+                        //注册成功
+                        ctx.body = {
+                            data: 3
+                        };
+                    })
+            }
+        })
 })
 module.exports = router
